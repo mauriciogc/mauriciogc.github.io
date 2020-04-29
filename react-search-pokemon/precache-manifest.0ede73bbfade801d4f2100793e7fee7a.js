18e523222a132fa22e755840c3a02b46cc4f1558
mauriciogc <mauriciogc@gmail.com>
2020-04-29 13:42:53 -0500

    move folder

---
+++ react-search-pokemon/precache-manifest.0ede73bbfade801d4f2100793e7fee7a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "186b2b5c1e1386a7eb292950ae251169",
+    "url": "/react-search-pokemon/index.html"
+  },
+  {
+    "revision": "b291ba0a94571c2ef83b",
+    "url": "/react-search-pokemon/static/css/main.a2378767.chunk.css"
+  },
+  {
+    "revision": "90bebe31d7c7e3cedea8",
+    "url": "/react-search-pokemon/static/js/2.ddf2c8bc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-search-pokemon/static/js/2.ddf2c8bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b291ba0a94571c2ef83b",
+    "url": "/react-search-pokemon/static/js/main.fed9091e.chunk.js"
+  },
+  {
+    "revision": "3789ed01a683505af970",
+    "url": "/react-search-pokemon/static/js/runtime-main.dd59735e.js"
+  },
+  {
+    "revision": "147ea4ab04a56e5630b20ba63ca4183f",
+    "url": "/react-search-pokemon/static/media/error.147ea4ab.gif"
+  },
+  {
+    "revision": "8bddf1c75ce5c6e6396e4a1dcf2fce0f",
+    "url": "/react-search-pokemon/static/media/loading.8bddf1c7.gif"
+  }
+]);
--- react-search-pokemon/service-worker.js
+++ react-search-pokemon/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3f4bcdf6d8462db603ade07e5ebb5434.js"
+  "/react-search-pokemon/precache-manifest.0ede73bbfade801d4f2100793e7fee7a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-search-pokemon/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
