cd3e53fde1d599271b8da8a901b2066a39114269
mauriciogc <mauriciogc@gmail.com>
2020-04-29 13:35:23 -0500

    Initial commit

---
+++ react-pokemon-search/precache-manifest.3f4bcdf6d8462db603ade07e5ebb5434.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd868d192ade41d7aecf0098bce761a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f55640c26b813e0cbc8",
+    "url": "/static/css/main.a2378767.chunk.css"
+  },
+  {
+    "revision": "90bebe31d7c7e3cedea8",
+    "url": "/static/js/2.ddf2c8bc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ddf2c8bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f55640c26b813e0cbc8",
+    "url": "/static/js/main.7134a476.chunk.js"
+  },
+  {
+    "revision": "852d4f86e85f1bc921cd",
+    "url": "/static/js/runtime-main.b59cae9e.js"
+  },
+  {
+    "revision": "147ea4ab04a56e5630b20ba63ca4183f",
+    "url": "/static/media/error.147ea4ab.gif"
+  },
+  {
+    "revision": "8bddf1c75ce5c6e6396e4a1dcf2fce0f",
+    "url": "/static/media/loading.8bddf1c7.gif"
+  }
+]);
